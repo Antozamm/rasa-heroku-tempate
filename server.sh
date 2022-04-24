a6a0cb3fb511d09312ec3a97860d6cb4b2a9517a
Anto Z <zantor72@gmail.com>
2022-04-24 16:59:21 +0200

    test

--- server.sh
+++ server.sh
@@ -9,4 +9,5 @@ fi
 rasa telemetry disable
 #rasa run --model models --enable-api --debug --port $PORT -models/nlu-20210516-154432.tar.gz
 #rasa run --enable-api -vv --debug --port $PORT -m models/20220102-221606.tar.gz
+
 rasa run --enable-api -vv --debug --port $PORT -m app/models/20220424-161830-independent-garage.tar.gz
